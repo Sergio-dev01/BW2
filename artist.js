1e3620d4744362646d2729c58773ff1bfdc8c8a5
roccadavide <davide.rocca03@gmail.com>
2025-05-07 11:00:25 +0200

    article even better

--- artist.js
+++ artist.js
@@ -41,16 +41,10 @@ const fetchArtistDetails = id => {
 };
 
 fetchArtistAlbums = id => {
-  const url = `https://deezerdevs-deezer.p.rapidapi.com/artist/${id}`;
-  const options = {
-    method: "GET",
-    headers: {
-      "x-rapidapi-key": token,
-      "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
-    },
-  };
+  // URL con il proxy di Strive School
+  const url = `https://striveschool-api.herokuapp.com/api/deezer/artist/${id}/top?limit=10`;
 
-  fetch(url, options)
+  fetch(url)
     .then(resp => {
       console.log(resp);
       if (!resp.ok) {
@@ -65,35 +59,36 @@ fetchArtistAlbums = id => {
     })
     .then(data => {
       console.log(data);
+
+      // Recupero l'elemento che conterrà gli album
       const albumList = document.getElementById("album-list");
-      albumList.innerHTML = "";
+      albumList.innerHTML = ""; // Pulisco prima di aggiungere nuovi risultati
 
-      const urlTrack = data.tracklist;
-      const optionsTrack = {
-        method: "GET",
-        headers: {
-          "x-rapidapi-key": token,
-          "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
-        },
-      };
+      data.data.forEach(track => {
+        // Creazione dell'elemento per ogni album
+        const col = document.createElement("div");
+        col.className = "col-12 px-1 mb-4";
 
-      fetch(urlTrack, optionsTrack)
-        .then(resp => {
-          console.log(resp);
-          if (!resp.ok) {
-            if (resp.status === 404) {
-              throw new Error("Risorsa non trovata");
-            } else if (resp.status >= 500) {
-              throw new Error("Errore lato server");
-            }
-            throw new Error("Errore nella fetch");
-          }
-          return resp.json();
-        })
-        .then(data => console.log(data))
-        .catch(error => {
-          console.log(error);
-        });
+        const card = document.createElement("div");
+        card.className = "p-3 pb-0";
+
+        const img = document.createElement("img");
+        img.className = "";
+        img.src = track.album.cover;
+
+        const div = document.createElement("div");
+        div.className = "card-body p-0 py-3";
+
+        const p = document.createElement("p");
+        p.className = "card-text text-white";
+        p.innerText = track.album.title;
+
+        div.appendChild(p);
+        card.appendChild(img);
+        card.appendChild(div);
+        col.appendChild(card);
+        albumList.appendChild(col);
+      });
     })
     .catch(error => {
       console.log(error);
