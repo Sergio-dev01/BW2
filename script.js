7dd8ded80df07fda2031450eb93c6a36af2e6c7e
roccadavide <davide.rocca03@gmail.com>
2025-05-08 14:15:34 +0200

    added over playbtn

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ mainFetch = (containerNum, start, end) => {
   };
 
   fetch(url, options)
-    .then((resp) => {
+    .then(resp => {
       console.log(resp);
       if (!resp.ok) {
         if (resp.status === 404) {
@@ -21,24 +21,52 @@ mainFetch = (containerNum, start, end) => {
       }
       return resp.json();
     })
-    .then((data) => {
+    .then(data => {
       console.log(data);
       const slicedData = data.data.slice(start, end);
       console.log(slicedData);
       const grid = document.querySelector(`.${containerNum}-main-grid-container`);
 
-      slicedData.forEach((artist) => {
+      slicedData.forEach(artist => {
         console.log(artist);
         console.log(artist.album);
         const col = document.createElement("div");
-        col.className = "col-12 col-sm-6 col-md-4 col-lg-2 px-1 mb-4";
+        col.className = "col-12 col-sm-6 col-md-4 col-lg-2 px-1 mb-4 cardCol";
 
         const card = document.createElement("div");
         card.className = "card p-3 pb-0 h-100";
 
+        const divSvg = document.createElement("div");
+        divSvg.className = "position-relative";
+
         const img = document.createElement("img");
         img.className = "card-img-top";
         img.src = artist.album.cover;
+
+        const btnWrapper = document.createElement("div");
+        btnWrapper.className = "position-absolute bottom-0 end-0 m-2 playBtn";
+
+        const playBtn = document.createElement("button");
+        playBtn.className = "btn btn-link p-0 rounded-circle d-flex justify-content-center align-items-center";
+        playBtn.style.width = "45px";
+        playBtn.style.height = "45px";
+        playBtn.style.backgroundColor = "#1bd760";
+
+        const playIcon = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+        playIcon.setAttribute("viewBox", "0 0 16 16");
+        playIcon.setAttribute("xmlns", "http://www.w3.org/2000/svg");
+        playIcon.setAttribute("fill", "black");
+        playIcon.style.width = "17px";
+        playIcon.style.height = "17px";
+
+        playIcon.innerHTML = `<path d="M3 1.713a.7.7 0 0 1 1.05-.607l10.89 6.288a.7.7 0 0 1 0 1.212L4.05 14.894A.7.7 0 0 1 3 14.288V1.713z" />`;
+
+        playBtn.appendChild(playIcon);
+        btnWrapper.appendChild(playBtn);
+
+        divSvg.appendChild(img);
+        divSvg.appendChild(btnWrapper);
+        card.appendChild(divSvg);
 
         const div = document.createElement("div");
         div.className = "card-body p-0 py-3";
@@ -48,13 +76,12 @@ mainFetch = (containerNum, start, end) => {
         p.innerText = artist.album.title;
 
         div.appendChild(p);
-        card.appendChild(img);
         card.appendChild(div);
         col.appendChild(card);
         grid.appendChild(col);
       });
     })
-    .catch((error) => {
+    .catch(error => {
       console.log(error);
     });
 };
@@ -70,8 +97,7 @@ upperMainFetch = () => {
   };
 
   fetch(url, options)
-    .then((resp) => {
-      console.log(resp);
+    .then(resp => {
       if (!resp.ok) {
         if (resp.status === 404) {
           throw new Error("Risorsa non trovata");
@@ -82,24 +108,44 @@ upperMainFetch = () => {
       }
       return resp.json();
     })
-    .then((data) => {
-      console.log(data);
+    .then(data => {
       const slicedData = data.data.slice(0, 8);
-      console.log(slicedData);
       const grid = document.querySelector(`.upper-main-grid-container`);
 
-      slicedData.forEach((artist) => {
-        console.log(artist);
-        console.log(artist.album);
+      slicedData.forEach(artist => {
         const col = document.createElement("div");
-        col.className = "col-6 col-sm-4 col-md-3 col-lg-3 px-1 mb-2";
+        col.className = "col-6 col-sm-4 col-md-3 col-lg-3 px-1 mb-2 cardCol";
 
         const card = document.createElement("div");
-        card.className = "pb-0 upperCard";
+        card.className = "pb-0 upperCard position-relative";
 
         const img = document.createElement("img");
         img.className = "upperCardImg";
         img.src = artist.album.cover;
+
+        const btnWrapper = document.createElement("div");
+        btnWrapper.className = "position-absolute end-0 top-50 translate-middle-y me-2 playBtn";
+
+        const playBtn = document.createElement("div");
+        playBtn.className = "btn btn-link p-0 rounded-circle d-flex justify-content-center align-items-center";
+        playBtn.style.width = "45px";
+        playBtn.style.height = "45px";
+        playBtn.style.backgroundColor = "#1bd760";
+
+        const playIcon = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+        playIcon.setAttribute("viewBox", "0 0 16 16");
+        playIcon.setAttribute("xmlns", "http://www.w3.org/2000/svg");
+        playIcon.setAttribute("fill", "black");
+        playIcon.style.width = "17px";
+        playIcon.style.height = "17px";
+
+        playIcon.innerHTML = `<path d="M3 1.713a.7.7 0 0 1 1.05-.607l10.89 6.288a.7.7 0 0 1 0 1.212L4.05 14.894A.7.7 0 0 1 3 14.288V1.713z" />`;
+
+        playBtn.appendChild(playIcon);
+        btnWrapper.appendChild(playBtn);
+
+        card.appendChild(img);
+        card.appendChild(btnWrapper);
 
         const div = document.createElement("div");
         div.className = "p-0 py-3";
@@ -109,13 +155,12 @@ upperMainFetch = () => {
         p.innerText = artist.album.title;
 
         div.appendChild(p);
-        card.appendChild(img);
         card.appendChild(div);
         col.appendChild(card);
         grid.appendChild(col);
       });
     })
-    .catch((error) => {
+    .catch(error => {
       console.log(error);
     });
 };
@@ -131,11 +176,11 @@ loadRandomAlbumRightbar = () => {
   };
 
   fetch(url, options)
-    .then((resp) => {
+    .then(resp => {
       if (!resp.ok) throw new Error("Errore nel recupero della rightbar");
       return resp.json();
     })
-    .then((data) => {
+    .then(data => {
       const albums = data.data;
       const rightbar = document.querySelector(".right-part");
 
@@ -181,7 +226,7 @@ loadRandomAlbumRightbar = () => {
         rightbar.appendChild(imageContainer);
       }
     })
-    .catch((error) => {
+    .catch(error => {
       console.error("Errore nella rightbar:", error);
     });
 };
